b057972122a48804f6d9e717f7263a7ed569ced1
Joe Wei <bigflywei@gmail.com>
2017-11-04 14:58:58 +0800

    First submission

---
+++ src/PID.cpp
@@ -0,0 +1,48 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd)
+{
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error = 0.0;
+
+	first = true;
+	prev_cte = 0.0;
+	sum_cte = 0.0;
+}
+
+void PID::UpdateError(double cte)
+{
+  if (first) {
+    first = false;
+    prev_cte = cte;
+  }
+
+  sum_cte += cte;
+
+  p_error = Kp * cte;
+  i_error = Ki * sum_cte;
+  d_error = Kd * (cte - prev_cte);
+  prev_cte = cte;
+
+}
+
+double PID::TotalError()
+{
+  return (p_error + i_error + d_error);
+}
+
